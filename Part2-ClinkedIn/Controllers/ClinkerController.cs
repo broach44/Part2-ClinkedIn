f4692665a5fd0a9f0c75f380e92810563e04be28
Crystal Broach <crysland@gmail.com>
2020-05-18 12:06:06 -0500

    Add project files.

---
+++ Part2-ClinkedIn/Controllers/ClinkerController.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Part2_ClinkedIn.DataAccessLayer;
+using Part2_ClinkedIn.Models;
+
+namespace Part2_ClinkedIn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClinkerController : ControllerBase
+    {
+        ClinkerRepo _repository = new ClinkerRepo();
+
+
+        [HttpPost]
+        public IActionResult AddClinker(Clinker clinkerToAdd)
+        {
+            var existingClinker = _repository.GetByFullName(clinkerToAdd);
+            if (existingClinker == null)
+            {
+                _repository.Add(clinkerToAdd);
+                return Created("", clinkerToAdd);
+            }
+            else
+            {
+                return BadRequest("User already exists, please try again.");
+            }
+        }
+
+        [HttpPost("services")]
+        public IActionResult CreateService(Services ServiceToAdd)
+        {
+            var existingService = _repository.CheckForService(ServiceToAdd);
+            if (existingService == null)
+            {
+                _repository.CreateService(ServiceToAdd);
+                return Ok(ServiceToAdd);
+            }
+            else
+            {
+                return BadRequest("This service already exists");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAllClinkers()
+        {
+            var allClinkers = _repository.GetAll();
+
+            return Ok(allClinkers);
+        }
+
+        // api/Clinker/{id}
+        // api/Clinker/5
+        [HttpGet("{id}")]
+        public IActionResult GetSingleClinker(int id)
+        {
+            var singleClinker = _repository.GetClinkerById(id);
+            if (singleClinker != null)
+            {
+                return Ok(singleClinker);
+            }
+            else
+            {
+                return NotFound("No clinker found");
+            }
+        }
+
+        // api/Clinker/searchByInterest/{interest}
+        // api/Clinker/searchByInterest/origamin
+        [HttpGet("searchByInterest/{interest}")]
+        public IActionResult GetByInterest(string interest)
+        {
+            var clinkersWithInterest = _repository.GetClinkerByInterest(interest);
+            var isEmpty = !clinkersWithInterest.Any();
+            if (!isEmpty)
+            {
+                return Ok(clinkersWithInterest);
+            }
+            else
+            {
+                return Ok("No Clinkers Found With Those Interests");
+            }
+        }
+
+
+        //api/Clinker/1/searchForPrisonTerm
+        //api/Clinker/{id}/searchForPrisonTerm
+        [HttpGet("{id}/searchForPrisonTerm")]
+        public IActionResult GetByPrisonTerm(int id)
+        {
+            var validClinker = _repository.GetClinkerById(id);
+            if (validClinker == null)
+            {
+                return BadRequest($"The user id {id} could not be found.");
+            }
+            else
+            {
+                var inmatePrisonTerm = _repository.GetClinkerByPrisonTerm(id);
+
+                return Ok(inmatePrisonTerm);
+            }
+
+        }
+
+
+
+        // api/Clinker/1/myFriends
+        // api/Clinker/{id}/myFriends
+        [HttpGet("{id}/myFriends")]
+        public IActionResult GetAllMyFriends(int id)
+        {
+            var allMyFriends = _repository.GetAllMyFriends(id);
+            var isEmpty = !allMyFriends.Any();
+
+            if (!isEmpty)
+            {
+                return Ok(allMyFriends);
+            }
+            else
+            {
+                return Ok("You have no friends!");
+            }
+
+        }
+
+        // api/Clinker/1/FriendsOfMyFriends
+        // api/Clinker/{id}/FriendsOfMyFriends
+        [HttpGet("{id}/FriendsOfMyFriends")]
+        public IActionResult GetAllFriendsOfMyFriends(int id)
+        {
+            var allMyFriends = _repository.AllFriendsOfFriends(id);
+            var isEmpty = !allMyFriends.Any();
+
+            if (!isEmpty)
+            {
+                return Ok(allMyFriends);
+            }
+            else
+            {
+                return Ok("Your friends have no friends");
+            }
+        }
+
+
+        //api/Clinker/1/addFriend/2       
+        [HttpPut("{clinkerId}/addFriend/{friendId}")]
+        public IActionResult UpdateClinkerFriends(int clinkerId, int friendId)
+        {
+            var updatedClinker = _repository.UpdateFriend(clinkerId, friendId);
+            if (updatedClinker != null)
+            {
+                return Ok(updatedClinker);
+            }
+            else
+            {
+                return BadRequest("The Clinker does not exist.");
+            }
+        }
+
+        //api/Clinker/1/addEnemy/2       
+        [HttpPut("{clinkerId}/addEnemy/{enemyId}")]
+        public IActionResult UpdateClinkerEnemies(int clinkerId, int enemyId)
+        {
+            var updatedClinker = _repository.UpdateEnemy(clinkerId, enemyId);
+            if (updatedClinker != null)
+            {
+                return Ok(updatedClinker);
+            }
+            else
+            {
+                return BadRequest("The Clinker does not exist.");
+            }
+        }
+
+        //api/Clinker/AddInterest/1/archery
+        [HttpPut("AddInterest/{id}/{interest}")]
+        public IActionResult AddInterest(int id, string interest)
+        {
+            var clinkerExists = _repository.GetClinkerById(id);
+            if (clinkerExists == null)
+            {
+                return BadRequest($"The user id {id} could not be found.");
+            }
+            else
+            {
+                var currentInterests = _repository.GetInterestsByClinkerId(id);
+                if (currentInterests.Contains(interest))
+                {
+                    return BadRequest($"{interest} is already on {clinkerExists.FirstName}'s interest list");
+                }
+                else
+                {
+                    _repository.CheckMasterInterestsAndUpdate(interest);
+                    clinkerExists.AddInterests(interest);
+                    return Ok(_repository.GetClinkerById(id));
+                }
+            }
+        }
+
+        //api/Clinker/RemoveInterest/1/archery
+        [HttpPut("RemoveInterest/{id}/{interest}")]
+        public IActionResult RemoveClinkerInterest(int id, string interest)
+        {
+            var clinkerExists = _repository.GetClinkerById(id);
+            if (clinkerExists == null)
+            {
+                return BadRequest($"The user id {id} could not be found.");
+            }
+            else
+            {
+                var currentInterests = _repository.GetInterestsByClinkerId(id);
+                if (currentInterests.Contains(interest))
+                {
+                    clinkerExists.RemoveInterests(interest);
+                    return Ok(_repository.GetClinkerById(id));
+                }
+                else
+                {
+                    return BadRequest($"{interest} was not found on {clinkerExists.FirstName}'s interest list");
+
+                }
+            }
+        }
+
+        //api/Clinker/AddService/1
+        [HttpPut("AddService/{clinkerId}")]
+        public IActionResult AddService(int clinkerId, Services serviceToAdd)
+        {
+            var clinkerExists = _repository.GetClinkerById(clinkerId);
+            if (clinkerExists == null)
+            {
+                return BadRequest($"The user id {clinkerId} could not be found.");
+            }
+            else
+            {
+                var currentClinkerService = _repository.GetServicesByClinkerId(clinkerId);
+                if (currentClinkerService.FirstOrDefault(s => s.Name.ToLower() == serviceToAdd.Name.ToLower()) != null)
+                {
+                    return BadRequest($"{serviceToAdd.Name} is already on {clinkerExists.FirstName}'s service list");
+                }
+                else
+                {
+                    var currentService = _repository.CheckForService(serviceToAdd);
+                    if (currentService == null)
+                    {
+                        _repository.CreateService(serviceToAdd);
+                        var newService = _repository.CheckForService(serviceToAdd);
+                        clinkerExists.AddService(newService);
+                    }
+                    else
+                    {
+                        clinkerExists.AddService(currentService);
+                    }
+                    return Ok(_repository.GetClinkerById(clinkerId));
+                }
+            }
+        }
+
+        //api/Clinker/RemoveService/1
+        [HttpPut("RemoveService/{clinkerId}")]
+        public IActionResult RemoveClinkerService(int clinkerId, Services serviceToRemove)
+        {
+            var clinkerExists = _repository.GetClinkerById(clinkerId);
+            if (clinkerExists == null)
+            {
+                return BadRequest($"The user id {clinkerId} could not be found.");
+            }
+            else
+            {
+                var currentClinkerServices = _repository.GetServicesByClinkerId(clinkerId);
+                var matchToCurrentService = currentClinkerServices.FirstOrDefault(s => s.Name.ToLower() == serviceToRemove.Name.ToLower());
+                if (matchToCurrentService == null)
+                {
+                    return BadRequest($"{serviceToRemove.Name} was not found on {clinkerExists.FirstName}'s service list");
+                }
+                else
+                {
+                    clinkerExists.RemoveService(matchToCurrentService);
+                    return Ok(_repository.GetClinkerById(clinkerId));
+                }
+            }
+        }
+
+    }
+}
---
+++ Part2-ClinkedIn/DataAccessLayer/ClinkerRepo.cs
@@ -0,0 +1,176 @@
+﻿using Part2_ClinkedIn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Part2_ClinkedIn.DataAccessLayer
+{
+    public class ClinkerRepo
+    {
+
+
+        static List<Clinker> _clinkers = new List<Clinker>()
+        {
+
+            new Clinker { Id = 1, FirstName = "John", LastName = "Doe", PrisonTermEndDate = new DateTime(2035, 03, 24), Interests = new List<string>() { "Killin" } },
+            new Clinker { Id = 2, FirstName = "Jimmie", LastName = "John", PrisonTermEndDate = new DateTime(2050, 04, 12), Interests = new List<string>() { "Killin" } },
+            new Clinker { Id = 3, FirstName = "Sam", LastName = "Smith", PrisonTermEndDate = new DateTime(2025, 06, 19), Interests = new List<string>() { "BeatBoxin" } },
+            new Clinker { Id = 4, FirstName = "Samson", LastName = "Smith", PrisonTermEndDate = new DateTime(2029, 01, 31), Interests = new List<string>() { "BeatBoxin" } },
+            new Clinker {Id = 5, FirstName = "ButterCup", LastName = "Johnson", PrisonTermEndDate = new DateTime(2024, 02, 15), Interests = new List<string>() {"BasketWeavin", "Origamin" } },
+            new Clinker {Id = 6, FirstName = "Slash", LastName = "MacGruber", PrisonTermEndDate = new DateTime(2030, 10, 31), Interests = new List<string>() { "Killin", "BasketWeavin" }},
+            new Clinker {Id = 7, FirstName = "Slick", LastName = "Willie", PrisonTermEndDate = new DateTime(2021, 09, 22), Interests = new List<string>() { "Origamin" }},
+            new Clinker {Id = 8, FirstName = "LittleShoe", LastName = "Wilomena", PrisonTermEndDate = new DateTime(2031, 12, 18), Interests = new List<string>() { "Killin", "BeatBoxin", "Origamin" } },
+
+
+        };
+
+        static List<Services> _services = new List<Services>()
+        {
+            new Services { Id = 1, Name= "Hair Cut", Price = 3.00 },
+            new Services { Id = 2, Name= "Legal Advice", Price = 5.00 },
+            new Services { Id = 3, Name= "Cuddle Buddy", Price = 2.00 }
+        };
+
+        static List<string> _interests = new List<string>()
+        {
+            "ShowTunesin",
+            "Killin",
+            "BeatBoxin",
+            "BasketWeavin",
+            "Origamin"
+        };
+
+        public Clinker GetByFullName(Clinker clinkerToAdd)
+        {
+            var firstNameMatch = _clinkers.FindAll(c => c.FirstName == clinkerToAdd.FirstName);
+            return firstNameMatch.FirstOrDefault(clinker => clinker.LastName == clinkerToAdd.LastName);
+        }
+
+        public void Add(Clinker clinker)
+        {
+            clinker.Id = _clinkers.Max(i => i.Id) + 1;
+            _clinkers.Add(clinker);
+        }
+
+        public List<Clinker> GetAll()
+        {
+            return _clinkers;
+        }
+
+        public List<Clinker> GetAllMyFriends(int clinkerId)
+        {
+            return GetClinkerById(clinkerId).Friends;
+        }
+
+        public List<Clinker> AllFriendsOfFriends(int clinkerId)
+        {
+            var clinkersFriends = GetAllMyFriends(clinkerId);
+            if (clinkersFriends != null)
+            {
+                foreach (var friend in clinkersFriends)
+                {
+                    return GetAllMyFriends(friend.Id);
+                }
+            }
+            return clinkersFriends;
+        }
+
+        public Clinker GetClinkerById(int clinkerId)
+        {
+            return _clinkers.FirstOrDefault(c => c.Id == clinkerId);
+        }
+
+        public void CheckMasterInterestsAndUpdate(string newInterest)
+        {
+            var existingInterest = _interests.FirstOrDefault(i => i.ToLower() == newInterest.ToLower());
+            if (existingInterest == null)
+            {
+                _interests.Add(newInterest);
+            }
+        }
+
+        public List<Clinker> GetClinkerByInterest(string interest)
+        {
+            var clinkerInterestMatch = _clinkers.FindAll(x => x.Interests.Contains(interest));
+
+            return clinkerInterestMatch;
+        }
+
+        public int CalculatePrisonTerm(DateTime endDate)
+        {
+
+            var numberOfDaysLeft = endDate.Date.Subtract(DateTime.Now.Date).Days;
+            return numberOfDaysLeft;
+        }
+
+        public int GetClinkerByPrisonTerm(int id)
+
+        {
+
+            var clinkerPrisonTerm = GetClinkerById(id).PrisonTermEndDate;
+            var numberOfDaysRemaining = CalculatePrisonTerm(clinkerPrisonTerm);
+            return numberOfDaysRemaining;
+
+        }
+
+
+
+        public List<string> GetInterestsByClinkerId(int id)
+        {
+            var targetClinker = GetClinkerById(id);
+            return targetClinker.Interests;
+        }
+
+        public Services CheckForService(Services newService)
+        {
+            return _services.FirstOrDefault(s => s.Name.ToLower() == newService.Name.ToLower());
+        }
+
+        public void CreateService(Services newService)
+
+        {
+            newService.Id = _services.Max(i => i.Id) + 1;
+            _services.Add(newService);
+        }
+
+
+        public List<Services> GetServicesByClinkerId(int id)
+        {
+            var targetClinker = GetClinkerById(id);
+            return targetClinker.Services;
+        }
+
+
+
+        public Clinker UpdateFriend(int clinkerToUpdateId, int clinkerToAddId)
+        {
+            var newClinkerFriend = GetClinkerById(clinkerToAddId);
+            var clinkerToUpdate = GetClinkerById(clinkerToUpdateId);
+            if (newClinkerFriend != null && clinkerToUpdate != null)
+            {
+                clinkerToUpdate.AddNewFriend(newClinkerFriend);
+                return clinkerToUpdate;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Clinker UpdateEnemy(int clinkerToUpdateId, int clinkerToAddId)
+        {
+            var newClinkerEnemy = GetClinkerById(clinkerToAddId);
+            var clinkerToUpdate = GetClinkerById(clinkerToUpdateId);
+            if (newClinkerEnemy != null && clinkerToUpdate != null)
+            {
+                clinkerToUpdate.AddNewEnemy(newClinkerEnemy);
+                return clinkerToUpdate;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
